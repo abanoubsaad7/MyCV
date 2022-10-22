1e8727b631d257c28fb2fdcf02592f738c6aaa2b
Abanoub Saad <88698601+abanoubsaad7@users.noreply.github.com>
2022-10-23 00:43:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#cpp').click(function(){
+      $('.cpp').fadeToggle();
+    });
+    $('#JS').click(function(){
+        $('.js').fadeToggle();
+    });
+    $('#front').click(function(){
+    $('.front').fadeToggle();    
+    });
+    $('#gmail').click(function(){
+        $('p.gmail').fadeToggle();    
+        });
+    $('#whats').click(function(){
+        $('p.whats').fadeToggle();    
+        });
+  });
